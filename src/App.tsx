4cad7e04ee1ca5a2759350753040f519b9c3e404
dimamaksymiuk01 <admin@admins-MacBook-Air.local>
2023-07-26 08:38:03 +0300

    addRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Login from "./pages/login/Login.tsx";
+import Archive from "./pages/archive/Archive.tsx";
+import Markets from "./pages/markets/Markets.tsx";
+import Storage from "./pages/storage/Storage.tsx";
+import Repairs from "./pages/repairs/Repairs.tsx";
+
 
 function App() {
 
   return (
     <>
-        <h2>Clear project</h2>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/markets" element={<Markets/>} />
+                <Route path="/archive" element={<Archive/>} />
+                <Route path="/storage" element={<Storage/>} />
+                <Route path="/repairs" element={<Repairs/>} />
+                <Route path='*' element={<Navigate to="/login" />} />
+            </Routes>
+          </Router>
     </>
   )
 }
