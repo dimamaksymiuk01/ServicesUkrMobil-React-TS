947065e9c6cf07d3d85ba85960082cdb0e54e94e
dimamaksymiuk01 <admin@admins-MacBook-Air.local>
2023-07-29 00:02:17 +0300

    createPageNotFound

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,9 +1,50 @@
+import '../../components/style/notFound.scss'
+import {Link} from "react-router-dom";
 function NotFound() {
 
   return (
-    <>
-        <h2>Not Found</h2>
-    </>
+    <div className={'notFound'}>
+      <header className="top-header"></header>
+
+      {/* Dust particle */}
+      <div>
+        <div className="starsec"></div>
+        <div className="starthird"></div>
+        <div className="starfourth"></div>
+        <div className="starfifth"></div>
+      </div>
+      {/* Dust particle end */}
+
+      <div className="lamp__wrap">
+        <div className="lamp">
+          <div className="cable"></div>
+          <div className="cover"></div>
+          <div className="in-cover">
+            <div className="bulb"></div>
+          </div>
+          <div className="light"></div>
+        </div>
+      </div>
+      {/* END Lamp */}
+      <section className="error">
+        {/* Content */}
+        <div className="error__content">
+          <div className="error__message message">
+            <h1 className="message__title">Page Not Found</h1>
+            <p className="message__text">
+              We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.
+            </p>
+          </div>
+          <div className="error__nav e-nav">
+            {/*<link to="/login" className="e-nav__link"></link>*/}
+              <Link to="/login">
+            <button className="e-nav__link"></button>
+              </Link>
+          </div>
+        </div>
+        {/* END Content */}
+      </section>
+    </div>
   )
 }
 
